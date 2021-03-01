9a210e9fa6dcaa21d04b5f57c1412e1f7022caaf
tanganyu <569386693@qq.com>
2021-03-01 16:51:26 +0800

    读取ini配置文件信息value值

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"gopkg.in/ini.v1"
+	"strings"
+)
+
+var (
+	iniFiles    string
+	sectionName string
+	keyName     string
+)
+
+func init() {
+	flag.StringVar(&iniFiles, "f", "", "Plz enter the ini file one or more;eg:one.ini,two.ini")
+	flag.StringVar(&sectionName, "s", "", "Plz enter the sectionName")
+	flag.StringVar(&keyName, "k", "", "Plz enter the keyName")
+	flag.Parse()
+}
+
+func main() {
+	files := strings.Split(iniFiles, ",")
+	filei := make([]interface{}, 0)
+	for _, file := range files {
+		filei = append(filei, file)
+	}
+
+	cfg, err := ini.LoadSources(ini.LoadOptions{
+		SkipUnrecognizableLines: true,
+	}, files[0], filei[1:]...)
+	if err != nil {
+		fmt.Println(err)
+	}
+	val := cfg.Section(sectionName).Key(keyName).Value()
+	fmt.Println(val)
+}
